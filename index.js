d8e9fb962c84cf8200007d98892757e9350b2077
Massimiliano Cannarozzo <maxcanna@gmail.com>
2018-07-13 23:23:13 +0200

    Add Google Assistant route

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
     , api = require('./routes_api')
+    , assistant = require('./routes_assistant')
     , { env: { PORT: port = 3000 } } = process
     , app = express();
 
 app.use('/api', api);
+app.use('/assistant', assistant);
 app.use(express.static(__dirname + '/public'));
 app.disable('x-powered-by');
 app.enable('trust proxy');
