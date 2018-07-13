0fec8b2ee70a27684d945200cdcab4153156e05f
Massimiliano Cannarozzo <maxcanna@gmail.com>
2018-07-13 23:24:22 +0200

    Remove usage of app.set

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ app.disable('x-powered-by');
 app.enable('trust proxy');
 app.use(require('compression')());
 app.use(require('morgan')('combined'));
-app.set('port', port);
 
-app.listen(app.get('port'), () => console.log(`bicineabbiamo listening on port ${app.get('port')}`));
+app.listen(port, () => console.log(`bicineabbiamo listening on port ${port}`));
